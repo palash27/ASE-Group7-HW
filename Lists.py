5bd58619f6e9d40404eb83ae5edc3c3c6cad1881
Rohan Sinha <sinharohan3@gmail.com>
2023-01-17 18:59:20 -0500

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -11,3 +11,10 @@ def kap(t,fun):
         v,k = fun(k,v)
         u[k or (1 + len(u))] = v
     return u
+
+def sort(t, fun=None):
+    if fun is None:
+        fun = lambda x: x[0]
+    tmp = sorted(t.items(), key=fun)
+    t = dict(tmp)
+    return t
