51e294b9695ce27f82b7a3bf38b980b642043ce5
Dev Kumar <devskumar99@gmail.com>
2023-01-25 13:29:35 -0500

    adding documentation to repo
    Signed-off-by: Dev Kumar <devskumar99@gmail.com>

--- src/Sym.py
+++ src/Sym.py
@@ -1,6 +1,9 @@
 import math
 
 class Sym:
+    """
+    Summarize a stream of symbols.
+    """
     def __init__(self) -> None:
         self.n = 0
         self.has = {}
@@ -8,6 +11,9 @@ class Sym:
         self.mode = None
         
     def add(self, x):
+        """
+        update counts of things seen so far
+        """
         if x != '?':
             self.n = self.n + 1
             self.has[x] = 1 + self.has.get(x, 0)
@@ -15,10 +21,19 @@ class Sym:
                 self.most, self.mode = self.has[x], x
 
     def mid(self):
+        """
+        return the mode
+        """
         return self.mode
 
     def div(self):
+        """
+        return the entropy
+        """
         def fun(p):
+            """
+            pass `p` and return p time log of p to the base 2
+            """
             return p*math.log(p,2)
         e = 0
         for _,n in self.has.items():
